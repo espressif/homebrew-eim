3ab9f2c161ada091bf6e707cf905ebd9aaa01750
GitHub Action <action@github.com>
2025-09-26 09:24:11 +0000

    Update eim formula and cask to version v0.5.1-test-release

--- Casks/eim-gui.rb
+++ Casks/eim-gui.rb
@@ -1,12 +1,12 @@
 cask "eim-gui" do
-  version "v0.5.1-test"
+  version "v0.5.1-test-release"
 
   if Hardware::CPU.intel?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test/eim-gui-macos-x64.dmg"
-    sha256 "824bbcd4d8be0b49086d2155593a3d68f406783abfbf3bc9c26f55a829ebefee"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-gui-macos-x64.dmg"
+    sha256 "5b7ef1b690aa429f18415bd5de5abbb8460687baf1e0c55a43963ebfa85aed05"
   elsif Hardware::CPU.arm?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test/eim-gui-macos-aarch64.dmg"
-    sha256 "0f8ad8b70100b49afefa5d5453804c222bf05a85585388fa070c21f4b772a6fe"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-gui-macos-aarch64.dmg"
+    sha256 "d7fcd490434417bf16641c9f957f7717f26522074735782260b49d7e7ec51ef1"
   end
 
   name "ESP-IDF Installer and Manager"
--- Formula/eim.rb
+++ Formula/eim.rb
@@ -1,14 +1,14 @@
 class Eim < Formula
   desc "ESP-IDF Installer and Manager CLI"
   homepage "https://github.com/espressif/idf-im-ui"
-  version "v0.5.1-test"
+  version "v0.5.1-test-release"
 
   if Hardware::CPU.intel?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test/eim-cli-macos-x64.zip"
-    sha256 "be7e5d760034baf51f6a8912078e96f0d83de1273a4f293401621f1dd5b51714"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-cli-macos-x64.zip"
+    sha256 "a0391c2869858f6f9c414e6272783b4141b5dbfa00049cc2cea2c2666f37d8c7"
   elsif Hardware::CPU.arm?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test/eim-cli-macos-aarch64.zip"
-    sha256 "ad2840ef0209dae11e4ca1a1c60c36c3824773b3f2400c10ea6132257bfd4826"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-cli-macos-aarch64.zip"
+    sha256 "19363ad0ec65b355491beb818c5412ca12def3bcfdb9437df4b5e9cde6c0f3de"
   end
 
   def install
