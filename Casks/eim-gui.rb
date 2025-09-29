f8179741ae30169513b0aa8bbc5a5fe4745328de
GitHub Action <action@github.com>
2025-09-29 10:34:05 +0000

    Update eim formula and cask to version v0.5.1-test-release

--- Casks/eim-gui.rb
+++ Casks/eim-gui.rb
@@ -3,10 +3,10 @@ cask "eim-gui" do
 
   if Hardware::CPU.intel?
     url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-gui-macos-x64.dmg"
-    sha256 "5b7ef1b690aa429f18415bd5de5abbb8460687baf1e0c55a43963ebfa85aed05"
+    sha256 "f88ddefef7074fbb33470781b4be835dccace498a4879ff900768a043eb34251"
   elsif Hardware::CPU.arm?
     url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-gui-macos-aarch64.dmg"
-    sha256 "d7fcd490434417bf16641c9f957f7717f26522074735782260b49d7e7ec51ef1"
+    sha256 "9e4af507d74906166c179266059bb1c724bbc837ff406f0e6d4416aabff885ac"
   end
 
   name "ESP-IDF Installer and Manager"
--- Formula/eim.rb
+++ Formula/eim.rb
@@ -5,10 +5,10 @@ class Eim < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-cli-macos-x64.zip"
-    sha256 "a0391c2869858f6f9c414e6272783b4141b5dbfa00049cc2cea2c2666f37d8c7"
+    sha256 "86c027bd58ffa17dc4c23f3c8c7f69f62c28a4e907a39adde43a81658652a4e1"
   elsif Hardware::CPU.arm?
     url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.1-test-release/eim-cli-macos-aarch64.zip"
-    sha256 "19363ad0ec65b355491beb818c5412ca12def3bcfdb9437df4b5e9cde6c0f3de"
+    sha256 "f2d04a8b45fbaebfff68b3b9808b443e6ad0aa2d010c4c442e93bf51c149fb4b"
   end
 
   def install
