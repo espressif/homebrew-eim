610a237504ea16e9777f1b0571cb30f80c3618c4
GitHub Action <action@github.com>
2025-10-15 13:51:41 +0000

    Update eim formula and cask to version v0.5.5

--- Casks/eim-gui.rb
+++ Casks/eim-gui.rb
@@ -1,12 +1,12 @@
 cask "eim-gui" do
-  version "v0.5.4"
+  version "v0.5.5"
 
   if Hardware::CPU.intel?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.4/eim-gui-macos-x64.dmg"
-    sha256 "11779ed1a7a1190b5ffd81eaf88e99a999495e214614596d28ab129f993de73f"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.5/eim-gui-macos-x64.dmg"
+    sha256 "b6b32df143a156c02176a603762bb6be3625f9f1ae944c5c0ae3dc37e7d7c69c"
   elsif Hardware::CPU.arm?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.4/eim-gui-macos-aarch64.dmg"
-    sha256 "6155fc1d3dbcb2511a0f5a65f730269170371eb7ec710edda66104bed1a71dc8"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.5/eim-gui-macos-aarch64.dmg"
+    sha256 "737807e7ce490a7eb44f304b67e33ca7aad12bb7566370426483257634ac77eb"
   end
 
   name "ESP-IDF Installer and Manager"
--- Formula/eim.rb
+++ Formula/eim.rb
@@ -1,14 +1,14 @@
 class Eim < Formula
   desc "ESP-IDF Installer and Manager CLI"
   homepage "https://github.com/espressif/idf-im-ui"
-  version "v0.5.4"
+  version "v0.5.5"
 
   if Hardware::CPU.intel?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.4/eim-cli-macos-x64.zip"
-    sha256 "eb12c1e5e8eee9f47df5ce82e2c9c9afb46470f255f66262fe0e39262c430773"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.5/eim-cli-macos-x64.zip"
+    sha256 "7106549c1b038e1b099b9f5958a16535a0b4af8df280fc874f7545c2fe16efcd"
   elsif Hardware::CPU.arm?
-    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.4/eim-cli-macos-aarch64.zip"
-    sha256 "f72d16e9869917c6720d31712b68f695482e26cf3cb4bbb91788270a3e41dd5a"
+    url "https://github.com/espressif/idf-im-ui/releases/download/v0.5.5/eim-cli-macos-aarch64.zip"
+    sha256 "4245e55c997b06ece3bce9da5f7f2261e1e0d42999651379a84ccf09fbae20de"
   end
 
   def install
